7ad480e1ffd58e473228bd00e6a740c5df02d129
Grizvok <burninglovedesign@gmail.com>
2018-05-30 11:24:44 -0400

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+
+
+import './App.css';
+
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import { MenuItem } from 'react-bootstrap';
+
+
+class App extends Component {
+  state = {
+    response: ''
+  };
+ 
+
+  render() {
+    return (
+
+      <Navbar bsStyle='default' inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a className="brand" href="#brand">Twitchthroe</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        Login
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Register
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+    );
+  }
+}
+
+export default App;
